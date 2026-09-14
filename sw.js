630359db2745a02248529f44e9d4ccb9069e7eb2
m7mouz-maker <m7mouz@gmail.com>
2026-09-14 15:16:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "my-shuffler-v3";
+const CACHE = "my-shuffler-v4";
 const ASSETS = ["./","./index.html","./shows.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
